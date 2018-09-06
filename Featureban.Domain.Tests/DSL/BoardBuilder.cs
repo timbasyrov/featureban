cb79464a9bcbccece5c48b81536bb3c05ff15457
Timur Basyrov <timmthetiger@mail.ru>
2018-09-07 00:21:16 +0500

    Implement BoardBuilder in DSL

---
+++ Featureban.Domain.Tests/DSL/BoardBuilder.cs
@@ -0,0 +1,19 @@
+﻿namespace Featureban.Domain.Tests.DSL
+{
+    public class BoardBuilder
+    {
+        private uint _wipLimit;
+
+        public BoardBuilder WithLimit(uint wipLimit)
+        {
+            _wipLimit = wipLimit;
+            return this;
+        }
+
+        public Board Please()
+        {
+            return new Board(_wipLimit);
+        }
+
+    }
+}
